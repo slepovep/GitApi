ef3e6a00e5334998703f4c811ae2888764106d3b
slepovep <slepov.ep@gmail.com>
2021-02-18 14:58:25 +0300

    добавлен параметр HeaderBase64 добавлена функция декодирования commitmes и directory из Base64

--- GitApi/Controllers/ObjectToGit.cs
+++ GitApi/Controllers/ObjectToGit.cs
@@ -28,146 +28,163 @@ namespace GitApi.Controllers
         [HttpPost]
         public string ObjectPush()
         {
-            OracleObect oracleobect = new OracleObect();
-            var pathGit = Configuration["PathGit"];
-            var urlGit = Configuration["UrlGit"];
-            var keyserver = Configuration["ApiKey"];  //parusyp to MD5 
-
-            //считывание параметров из заголовка запроса
-            oracleobect.NameDB = Request.Headers["NameDB"].ToString();
-            oracleobect.NameObject = Request.Headers["NameObject"].ToString();
-            string apikey = Request.Headers["ApiKey"].ToString();
-            string username = "\"" + Request.Headers["UserName"].ToString() + "\"";
-            string usermail = "\"" + Request.Headers["UserMail"].ToString() + "\"";
-            string commitmes = "\"" + Request.Headers["CommitMes"].ToString() + "\"";
-            string directory = Request.Headers["Directory"].ToString();
-            string path = pathGit + oracleobect.NameDB;
-            string repository = path;
-            string writePath = "";
-
-            if (!string.IsNullOrEmpty(directory)) 
-            {
-                path = path + "\\" + directory + @"\";
-                writePath = path + oracleobect.NameObject;
-            }
-            else
-			{
-                path = path + @"\";
-                writePath = path + @"\" + oracleobect.NameObject; 
-            }
-
-            repository = repository.Replace("\\", @"\"); // каталог к git repository
             string result = "OK";
-            string commithash = "";
-            string gitcommands = "";
-            string gitoutput = "";
-            string stroutput = "";
 
-            //проверка APIKEY
-            if (keyserver != apikey)
-            {
-                result = "Error ApiKey " + apikey;
-                return result;
-            }
-
-            //создание файла на диске
             try
-            {
-                //поиск и создание каталога на диске
-                DirectoryInfo dirInfo = new DirectoryInfo(path);
-                if (!dirInfo.Exists)
+            {            
+                OracleObect oracleobect = new OracleObect();
+                var pathGit = Configuration["PathGit"];
+                var urlGit = Configuration["UrlGit"];
+                var keyserver = Configuration["ApiKey"];  //parusyp to MD5 
+
+                //считывание параметров из заголовка запроса
+                string headercoding = Request.Headers["HeaderBase64"].ToString();  //кодировка заголовка
+                oracleobect.NameDB = Request.Headers["NameDB"].ToString();
+                oracleobect.NameObject = Request.Headers["NameObject"].ToString();
+            
+                string apikey = Request.Headers["ApiKey"].ToString();
+                string username = "\"" + Request.Headers["UserName"].ToString() + "\"";
+                string usermail = "\"" + Request.Headers["UserMail"].ToString() + "\"";
+                string commitmes = "\"" + Base64Decode(Request.Headers["CommitMes"].ToString(), headercoding) + "\"";
+                string directory = Base64Decode(Request.Headers["Directory"].ToString(), headercoding);
+                string path = pathGit + oracleobect.NameDB;
+                string repository = path;
+                string writePath = "";
+
+                if (!string.IsNullOrEmpty(directory)) 
                 {
-                    dirInfo.Create();
+                    path = path + "\\" + directory + @"\";
+                    writePath = path + oracleobect.NameObject;
+                }
+                else
+			    {
+                    path = path + @"\";
+                    writePath = path + @"\" + oracleobect.NameObject; 
                 }
 
-                //считывание тела запроса
-                using (var reader = new StreamReader(Request.Body))
-				{
-                    oracleobect.TextObject = reader.ReadToEnd();
-                }   
-                //запись на диск
-                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                repository = repository.Replace("\\", @"\"); // каталог к git repository
+                string commithash = "";
+                string gitcommands = "";
+                string gitoutput = "";
+                string stroutput = "";
+
+                //проверка APIKEY
+                if (keyserver != apikey)
                 {
-                    sw.WriteLine(oracleobect.TextObject);
-                    sw.Close();
+                    result = "Error ApiKey " + apikey;
+                    return result;
                 }
 
-                //отправка в репозиторий GIT
-                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-                startInfo.RedirectStandardError = true;
-                startInfo.FileName = "cmd.exe";
-                startInfo.Verb = "runas";
-                startInfo.WorkingDirectory = repository;
+                    //создание файла на диске
+                    //поиск и создание каталога на диске
+                    DirectoryInfo dirInfo = new DirectoryInfo(path);
+                    if (!dirInfo.Exists)
+                    {
+                        dirInfo.Create();
+                    }
+
+                    //считывание тела запроса
+                    using (var reader = new StreamReader(Request.Body))
+				    {
+                        oracleobect.TextObject = reader.ReadToEnd();
+                    }   
+                    //запись на диск
+                    using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                    {
+                        sw.WriteLine(oracleobect.TextObject);
+                        sw.Close();
+                    }
+
+                    //отправка в репозиторий GIT
+                    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                    startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                    startInfo.RedirectStandardError = true;
+                    startInfo.FileName = "cmd.exe";
+                    startInfo.Verb = "runas";
+                    startInfo.WorkingDirectory = repository;
    
 
-                gitcommands = @"/C git init" + " & " +
-                              "git config user.name " + username + " & " +
-                              "git config user.email " + usermail + " & " +
-                              "git add *" + " & " +
-                              "git commit -m " + commitmes + " & " +
-                              "git remote add " + oracleobect.NameDB + " " + urlGit + oracleobect.NameDB + ".git" + " & " + //SSH
-                              "git remote set-url " + oracleobect.NameDB + " " + urlGit + oracleobect.NameDB + ".git" + " & " + //SSH
-                              "git push " + oracleobect.NameDB + " master" + " & " +
-                              "git log -1 --pretty=format:" + "\"" + "%H" + "\"";
+                    gitcommands = @"/C git init" + " & " +
+                                  "git config user.name " + username + " & " +
+                                  "git config user.email " + usermail + " & " +
+                                  "git add *" + " & " +
+                                  "git commit -m " + commitmes + " & " +
+                                  "git remote add " + oracleobect.NameDB + " " + urlGit + oracleobect.NameDB + ".git" + " & " + //SSH
+                                  "git remote set-url " + oracleobect.NameDB + " " + urlGit + oracleobect.NameDB + ".git" + " & " + //SSH
+                                  "git push " + oracleobect.NameDB + " master" + " & " +
+                                  "git log -1 --pretty=format:" + "\"" + "%H" + "\"";
                 
-                startInfo.Arguments = gitcommands;
+                    startInfo.Arguments = gitcommands;
 
-                System.Diagnostics.Process process = new System.Diagnostics.Process();
-                process.StartInfo = startInfo;
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.RedirectStandardInput = true;
-                process.StartInfo.RedirectStandardOutput = true;
+                    System.Diagnostics.Process process = new System.Diagnostics.Process();
+                    process.StartInfo = startInfo;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardInput = true;
+                    process.StartInfo.RedirectStandardOutput = true;
 
-                process.Start();
+                    process.Start();
 
-                while (!process.StandardOutput.EndOfStream)
-                {
-                    stroutput = process.StandardOutput.ReadLine();
-                    gitoutput = gitoutput + " " + stroutput;
-                }
-                commithash = stroutput;
-                gitoutput = gitoutput.Substring(2);
-                string errors = process.StandardError.ReadToEnd();
-                process.WaitForExit();
-                process.Close();
-
-
-                result = result + '|' + commithash + '|' + gitoutput + '|' + errors + '|' + gitcommands;
-
-                /*  //использование PowerShell
-                using (PowerShell powershell = PowerShell.Create())
-                {
-                    // this changes from the user folder that PowerShell starts up with to your git repository
-                    powershell.AddScript($"cd {repository}");
-                    //авторизация
-                    powershell.AddScript(@"git config user.name " + username);
-                    powershell.AddScript(@"git config user.email " + usermail);
-
-                    powershell.AddScript(@"git init");
-                    powershell.AddScript(@"git add *");
-                    powershell.AddScript(@"git commit -m " + commitmes);
-                    //powershell.AddScript(@"git remote add "+ oracleobect.NameDB +" https://github.com/slepovep/" + oracleobect.NameDB + ".git");  //https
-                    powershell.AddScript(@"git remote set-url " + oracleobect.NameDB + urlGit + oracleobect.NameDB + ".git"); //SSH
-                    powershell.AddScript(@"git push "+ oracleobect.NameDB +" master");
-                    powershell.AddScript(@"git log -1 --pretty=format:" + "\"" + "%H" + "\"");
-                    //powershell.Invoke();
-
-                    var psresults = powershell.Invoke();
-                    foreach (var psresult in psresults)
+                    while (!process.StandardOutput.EndOfStream)
                     {
-                        commithash = psresult.ToString();
+                        stroutput = process.StandardOutput.ReadLine();
+                        gitoutput = gitoutput + " " + stroutput;
                     }
-                    result = result + '|' + commithash;
-                }*/
+                    commithash = stroutput;
+                    gitoutput = gitoutput.Substring(2);
+                    string errors = process.StandardError.ReadToEnd();
+                    process.WaitForExit();
+                    process.Close();
+
+
+                    result = result + '|' + commithash + '|' + gitoutput + '|' + errors + '|' + gitcommands;
+
+                    /*  //использование PowerShell
+                    using (PowerShell powershell = PowerShell.Create())
+                    {
+                        // this changes from the user folder that PowerShell starts up with to your git repository
+                        powershell.AddScript($"cd {repository}");
+                        //авторизация
+                        powershell.AddScript(@"git config user.name " + username);
+                        powershell.AddScript(@"git config user.email " + usermail);
+
+                        powershell.AddScript(@"git init");
+                        powershell.AddScript(@"git add *");
+                        powershell.AddScript(@"git commit -m " + commitmes);
+                        //powershell.AddScript(@"git remote add "+ oracleobect.NameDB +" https://github.com/slepovep/" + oracleobect.NameDB + ".git");  //https
+                        powershell.AddScript(@"git remote set-url " + oracleobect.NameDB + urlGit + oracleobect.NameDB + ".git"); //SSH
+                        powershell.AddScript(@"git push "+ oracleobect.NameDB +" master");
+                        powershell.AddScript(@"git log -1 --pretty=format:" + "\"" + "%H" + "\"");
+                        //powershell.Invoke();
+
+                        var psresults = powershell.Invoke();
+                        foreach (var psresult in psresults)
+                        {
+                            commithash = psresult.ToString();
+                        }
+                        result = result + '|' + commithash;
+                    }*/
 
             }
             catch (Exception ex)
             {
-                result = ex.Message + repository;
+                result = ex.Message;
                 //throw ex;
             }
             return result;
         }
+
+        public static string Base64Decode(string base64EncodedData, string headerCoding64)
+        {
+            if (headerCoding64 == "1")
+            {
+                var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+                return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+            }
+			else 
+            {
+                return base64EncodedData;
+            }
+        }
+
     }
 }
